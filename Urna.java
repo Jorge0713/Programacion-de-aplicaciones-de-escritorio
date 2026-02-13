b7a9ba3b6a74ce8a16b8655d95d80e23cf3d4952
Jorge Rafael Flores Gallegos <zS24016709@estudiantes.uv.mx>
2026-02-12 23:30:15 -0600

    Entregando

---
+++ Urna.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Urna {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] votos = new int[3]; // 3 candidatos
+        int voto;
+        int total = 0;
+
+        System.out.println("Ingrese voto (1-3). 0 para terminar:");
+        voto = sc.nextInt();
+
+        while (voto != 0) {
+
+            if (voto >= 1 && voto <= 3) {
+                votos[voto - 1]++;
+                total++;
+            } else {
+                System.out.println("Voto invalido");
+            }
+
+            voto = sc.nextInt();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            double porcentaje = (votos[i] * 100.0) / total;
+            System.out.println("Candidato " + (i + 1) + ": " + porcentaje + "%");
+        }
+
+        sc.close();
+    }
+}
+
